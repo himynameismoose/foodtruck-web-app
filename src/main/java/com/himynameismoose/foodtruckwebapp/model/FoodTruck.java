a942ebdf47180f4a68eedc8b15357b7eafa62048
Mershelle <mershellemae@gmail.com>
2022-06-08 16:15:05 -0700

    Created FoodTruck class -> create DB table

---
+++ src/main/java/com/himynameismoose/foodtruckwebapp/model/FoodTruck.java
@@ -0,0 +1,30 @@
+package com.himynameismoose.foodtruckwebapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "foodtrucks")
+public class FoodTruck {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "foodType")
+    private String foodType;
+}
