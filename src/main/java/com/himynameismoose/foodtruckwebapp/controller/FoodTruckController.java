996fee66c998e11efe3e4e7965b02978203f37ff
Mershelle <mershellemae@gmail.com>
2022-06-17 20:00:57 -0700

    Add @CrossOrigin to allow requests from React

--- src/main/java/com/himynameismoose/foodtruckwebapp/controller/FoodTruckController.java
+++ src/main/java/com/himynameismoose/foodtruckwebapp/controller/FoodTruckController.java
@@ -3,12 +3,14 @@ package com.himynameismoose.foodtruckwebapp.controller;
 import com.himynameismoose.foodtruckwebapp.model.FoodTruck;
 import com.himynameismoose.foodtruckwebapp.repository.FoodTruckRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/foodtrucks")
 public class FoodTruckController {
