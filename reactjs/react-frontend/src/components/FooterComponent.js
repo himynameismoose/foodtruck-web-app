314d4ec3dbd8f27fd24f756b29c6ac99d6c12dbb
Mershelle <mershellemae@gmail.com>
2022-06-18 13:14:39 -0700

    Create Footer Component

---
+++ reactjs/react-frontend/src/components/FooterComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterComponent = () => {
+    return (
+        <div>
+            <footer className="footer">
+                <span className="text-muted"> @himynameismoose </span>
+            </footer>
+        </div>
+    )
+}
