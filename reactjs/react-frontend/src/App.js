9fbdeeeb58aa9749c5a4a78ad4748f9ce7b297a3
Mershelle <mershellemae@gmail.com>
2022-06-20 15:13:26 -0700

    Add header and footer component to UI

--- reactjs/react-frontend/src/App.js
+++ reactjs/react-frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
 import ListFoodTruckComponent from './components/ListFoodTruckComponent';
 
 function App() {
   return (
     <div>
+      <HeaderComponent/>
       <ListFoodTruckComponent/>
+      <FooterComponent/>
     </div>
   );
 }
