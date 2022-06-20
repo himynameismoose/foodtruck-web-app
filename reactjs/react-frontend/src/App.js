b62f9b2040025d7b4c1e8fb66730c356618a1e04
Mershelle <mershellemae@gmail.com>
2022-06-20 12:05:01 -0700

    Create React from VS Code

--- reactjs/react-frontend/src/App.js
+++ reactjs/react-frontend/src/App.js
@@ -1,15 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import HeaderComponent from './components/HeaderComponent';
-import ListFoodTruckComponent from './components/ListFoodTruckComponent';
-import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
-    <div>
-        <HeaderComponent/>
-        <ListFoodTruckComponent/>
-        <FooterComponent/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
