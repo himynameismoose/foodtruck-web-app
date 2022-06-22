d112015b2ed0121c0d061839d296b7374442f7d8
Mershelle <mershellemae@gmail.com>
2022-06-22 14:14:54 -0700

    Add Food Truck Component

---
+++ reactjs/react-frontend/src/components/AddFoodTruckComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddFoodTruckComponent = () => {
+  return (
+    <div>
+        <h1> Add Food Truck Component Called </h1>
+    </div>
+  )
+}
+
+export default AddFoodTruckComponent
