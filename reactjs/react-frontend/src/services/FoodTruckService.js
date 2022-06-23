314ce45ea5e3737ba11bac164d6e0d88bc61f508
Mershelle <mershellemae@gmail.com>
2022-06-23 11:20:04 -0700

    Add Create food truck method to send to api

--- reactjs/react-frontend/src/services/FoodTruckService.js
+++ reactjs/react-frontend/src/services/FoodTruckService.js
@@ -3,8 +3,14 @@ import axios from 'axios';
 const FOODTRUCK_BASE_REST_API_URL = 'http://localhost:8080/api/v1/foodtrucks';
 
 class FoodTruckService {
+    // get all food trucks 
     getAllFoodTrucks() {
         return axios.get(FOODTRUCK_BASE_REST_API_URL);
+    }
+
+    // create food truck
+    createFoodTruck(foodtruck) {
+        return axios.post(FOODTRUCK_BASE_REST_API_URL, foodtruck);
     }
 }
 
